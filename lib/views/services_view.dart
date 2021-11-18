14245460146862ef5cf33c18ba2eee6ef8ebbe81
Usagi88 <ibrahimnaish5677@gmail.com>
2021-11-18 19:07:27 +0500

    service card view

--- lib/views/services_view.dart
+++ lib/views/services_view.dart
@@ -9,156 +9,164 @@ class ServicesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: Navbar(),
-      drawer: SideMenu(),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                height: 130,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(
-                        'assets/images/zakai.png'),
-                    fit: BoxFit.fill,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: Row(
-                  children:[
-                    Text(
-                      'Services',
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w700
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10.0),
-                      child: Container(
-                        height: 1,
-                        width: MediaQuery.of(context).size.width * 0.68,
-                        color: Colors.grey.shade400,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: Container(
-                    height: MediaQuery.of(context).size.height * 0.58,
-                    child: ListView(
-                      children: [
-                        Row(
-                          children: [
-                            ServiceCard(
-                              serviceCardImage: 'assets/images/dhiraagu-logo.png',
-                              serviceCardText: 'Dhiraagu Reload',
-                            ),
-                            SizedBox(
-                              width: 20,
-                            ),
-                            ServiceCard(
-                              serviceCardImage: 'assets/images/ooredoo-logo.png',
-                              serviceCardText: 'Ooredoo Raastas',
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          children: [
-                            ServiceCard(
-                              serviceCardImage: 'assets/images/dhiraagu-logo.png',
-                              serviceCardText: 'Dhiraagu Reload',
-                            ),
-                            SizedBox(
-                              width: 20,
-                            ),
-                            ServiceCard(
-                              serviceCardImage: 'assets/images/ooredoo-logo.png',
-                              serviceCardText: 'Ooredoo Raastas',
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          children: [
-                            ServiceCard(
-                              serviceCardImage: 'assets/images/dhiraagu-logo.png',
-                              serviceCardText: 'Dhiraagu Reload',
-                            ),
-                            SizedBox(
-                              width: 20,
-                            ),
-                            ServiceCard(
-                              serviceCardImage: 'assets/images/ooredoo-logo.png',
-                              serviceCardText: 'Ooredoo Raastas',
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          children: [
-                            ServiceCard(
-                              serviceCardImage: 'assets/images/dhiraagu-logo.png',
-                              serviceCardText: 'Dhiraagu Reload',
-                            ),
-                            SizedBox(
-                              width: 20,
-                            ),
-                            ServiceCard(
-                              serviceCardImage: 'assets/images/ooredoo-logo.png',
-                              serviceCardText: 'Ooredoo Raastas',
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          children: [
-                            ServiceCard(
-                              serviceCardImage: 'assets/images/dhiraagu-logo.png',
-                              serviceCardText: 'Dhiraagu Reload',
-                            ),
-                            SizedBox(
-                              width: 20,
-                            ),
-                            ServiceCard(
-                              serviceCardImage: 'assets/images/ooredoo-logo.png',
-                              serviceCardText: 'Ooredoo Raastas',
-                            ),
-                          ],
-                        ),
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return _portraitModeOnly(context);
 
-                      ],
-                    )
-                ),
-              ),
-
-            ],
-          )
-        )
-      )
-    );
   }
 }
 
+//portrait
+Scaffold _portraitModeOnly(BuildContext context) {
+  return Scaffold(
+      appBar: Navbar(),
+      drawer: SideMenu(),
+      body: SafeArea(
+          child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    height: 130,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                            'assets/images/zakai.png'),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Row(
+                      children:[
+                        Text(
+                          'Services',
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10.0),
+                          child: Container(
+                            height: 1,
+                            width: MediaQuery.of(context).size.width * 0.68,
+                            color: Colors.grey.shade400,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Container(
+                        height: MediaQuery.of(context).size.height * 0.58,
+                        child: ListView(
+                          children: [
+                            Row(
+                              children: [
+                                ServiceCard(
+                                  serviceCardImage: 'assets/images/dhiraagu-logo.png',
+                                  serviceCardText: 'Dhiraagu Reload',
+                                ),
+                                SizedBox(
+                                  width: 20,
+                                ),
+                                ServiceCard(
+                                  serviceCardImage: 'assets/images/ooredoo-logo.png',
+                                  serviceCardText: 'Ooredoo Raastas',
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Row(
+                              children: [
+                                ServiceCard(
+                                  serviceCardImage: 'assets/images/dhiraagu-logo.png',
+                                  serviceCardText: 'Dhiraagu Reload',
+                                ),
+                                SizedBox(
+                                  width: 20,
+                                ),
+                                ServiceCard(
+                                  serviceCardImage: 'assets/images/ooredoo-logo.png',
+                                  serviceCardText: 'Ooredoo Raastas',
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Row(
+                              children: [
+                                ServiceCard(
+                                  serviceCardImage: 'assets/images/dhiraagu-logo.png',
+                                  serviceCardText: 'Dhiraagu Reload',
+                                ),
+                                SizedBox(
+                                  width: 20,
+                                ),
+                                ServiceCard(
+                                  serviceCardImage: 'assets/images/ooredoo-logo.png',
+                                  serviceCardText: 'Ooredoo Raastas',
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Row(
+                              children: [
+                                ServiceCard(
+                                  serviceCardImage: 'assets/images/dhiraagu-logo.png',
+                                  serviceCardText: 'Dhiraagu Reload',
+                                ),
+                                SizedBox(
+                                  width: 20,
+                                ),
+                                ServiceCard(
+                                  serviceCardImage: 'assets/images/ooredoo-logo.png',
+                                  serviceCardText: 'Ooredoo Raastas',
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Row(
+                              children: [
+                                ServiceCard(
+                                  serviceCardImage: 'assets/images/dhiraagu-logo.png',
+                                  serviceCardText: 'Dhiraagu Reload',
+                                ),
+                                SizedBox(
+                                  width: 20,
+                                ),
+                                ServiceCard(
+                                  serviceCardImage: 'assets/images/ooredoo-logo.png',
+                                  serviceCardText: 'Ooredoo Raastas',
+                                ),
+                              ],
+                            ),
 
+                          ],
+                        )
+                    ),
+                  ),
+
+                ],
+              )
+          )
+      )
+  );
+}
