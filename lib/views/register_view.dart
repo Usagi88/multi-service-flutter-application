a54d773e63b15bfea337315bcda28269633620ef
Usagi88 <ibrahimnaish5677@gmail.com>
2021-12-21 20:55:41 +0500

    register view animations

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:fpapp/widgets/button_widget.dart';
 import 'package:fpapp/widgets/clickable_link_widget.dart';
@@ -7,8 +9,34 @@ import '/widgets/textfield_number_widget.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class RegisterView extends StatelessWidget {
+class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> with SingleTickerProviderStateMixin{
+  AnimationController? _animationController;
+  Tween<double> _tween = Tween(begin: 0.1, end: 1);
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+
+    );
+    Timer(Duration(milliseconds: 250), () => _animationController!.forward());
+    _animationController!.forward();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController!.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,12 +44,12 @@ class RegisterView extends StatelessWidget {
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
-    return _portraitModeOnly(context);
+    return _portraitModeOnly(context, _animationController, _tween);
   }
 }
 
 //portrait mode
-Scaffold _portraitModeOnly(BuildContext context) {
+Scaffold _portraitModeOnly(BuildContext context, _animationController, _tween) {
   return Scaffold(
     body: SafeArea(
       child: SingleChildScrollView(
@@ -32,17 +60,26 @@ Scaffold _portraitModeOnly(BuildContext context) {
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal:20.0),
-              child: Container(
-                //height: 150,
-                height: MediaQuery.of(context).size.height * 0.25,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
-                  ),
-                  image: DecorationImage(
-                    image: AssetImage(
-                        'assets/images/registration.png'),
-                    fit: BoxFit.fill,
+              child: SlideTransition(
+                position: Tween<Offset>(
+                  begin: Offset(0, -1),
+                  end: Offset.zero,
+                ).animate(_animationController),
+                child: FadeTransition(
+                  opacity: _animationController,
+                  child: Container(
+                    //height: 150,
+                    height: MediaQuery.of(context).size.height * 0.25,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                      image: DecorationImage(
+                        image: AssetImage(
+                            'assets/images/registration.png'),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
                   ),
                 ),
               ),
@@ -54,15 +91,24 @@ Scaffold _portraitModeOnly(BuildContext context) {
               padding: const EdgeInsets.only(left:8.0),
               child: Align(
                 alignment: Alignment.centerLeft,
-                child: Container(
-                  //height: 150,
-                  height: 34,
-                  width: 152,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage(
-                          'assets/images/fahipay-logo.png'),
-                      fit: BoxFit.fill,
+                child: SlideTransition(
+                  position: Tween<Offset>(
+                    begin: Offset(-1, 0),
+                    end: Offset.zero,
+                  ).animate(_animationController),
+                  child: FadeTransition(
+                    opacity: _animationController,
+                    child: Container(
+                      //height: 150,
+                      height: 34,
+                      width: 152,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(
+                              'assets/images/fahipay-logo.png'),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
                     ),
                   ),
                 ),
@@ -75,10 +121,19 @@ Scaffold _portraitModeOnly(BuildContext context) {
               padding: const EdgeInsets.only(left:20.0),
               child: Align(
                 alignment: Alignment.centerLeft,
-                child: Container(
-                  height: 1,
-                  width: MediaQuery.of(context).size.width * 0.50,
-                  color: Color(0xffE0E0E0),
+                child: SlideTransition(
+                  position: Tween<Offset>(
+                    begin: Offset(-1, 0),
+                    end: Offset.zero,
+                  ).animate(_animationController),
+                  child: FadeTransition(
+                    opacity: _animationController,
+                    child: Container(
+                      height: 1,
+                      width: MediaQuery.of(context).size.width * 0.50,
+                      color: Color(0xffE0E0E0),
+                    ),
+                  ),
                 ),
               ),
             ),
@@ -89,11 +144,20 @@ Scaffold _portraitModeOnly(BuildContext context) {
               padding: EdgeInsets.only(left:20.0),
               child: Align(
                 alignment: Alignment.centerLeft,
-                child: Text(
-                  AppLocalizations.of(context)!.register,
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w700
+                child: SlideTransition(
+                  position: Tween<Offset>(
+                    begin: Offset(-1, 0),
+                    end: Offset.zero,
+                  ).animate(_animationController),
+                  child: FadeTransition(
+                    opacity: _animationController,
+                    child: Text(
+                      AppLocalizations.of(context)!.register,
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700
+                      ),
+                    ),
                   ),
                 ),
               ),
@@ -105,8 +169,17 @@ Scaffold _portraitModeOnly(BuildContext context) {
               padding: EdgeInsets.symmetric(horizontal:20.0),
               child: Align(
                 alignment: Alignment.centerLeft,
-                child: TextfieldTextWidget(
-                  hintText: AppLocalizations.of(context)!.idCardNumber,
+                child: SlideTransition(
+                  position: Tween<Offset>(
+                    begin: Offset(2, 0),
+                    end: Offset.zero,
+                  ).animate(_animationController),
+                  child: FadeTransition(
+                    opacity: _animationController,
+                    child: TextfieldTextWidget(
+                      hintText: AppLocalizations.of(context)!.idCardNumber,
+                    ),
+                  ),
                 ),
               ),
             ),
@@ -117,8 +190,17 @@ Scaffold _portraitModeOnly(BuildContext context) {
               padding: EdgeInsets.symmetric(horizontal:20.0),
               child: Align(
                 alignment: Alignment.centerLeft,
-                child: TextfieldTextWidget(
-                  hintText: AppLocalizations.of(context)!.fullName,
+                child: SlideTransition(
+                  position: Tween<Offset>(
+                    begin: Offset(-1, 0),
+                    end: Offset.zero,
+                  ).animate(_animationController),
+                  child: FadeTransition(
+                    opacity: _animationController,
+                    child: TextfieldTextWidget(
+                      hintText: AppLocalizations.of(context)!.fullName,
+                    ),
+                  ),
                 ),
               ),
             ),
@@ -129,8 +211,17 @@ Scaffold _portraitModeOnly(BuildContext context) {
               padding: EdgeInsets.symmetric(horizontal:20.0),
               child: Align(
                 alignment: Alignment.centerLeft,
-                child: TextfieldNumberWidget(
-                  hintText: AppLocalizations.of(context)!.mobileNumber,
+                child: SlideTransition(
+                  position: Tween<Offset>(
+                    begin: Offset(2, 0),
+                    end: Offset.zero,
+                  ).animate(_animationController),
+                  child: FadeTransition(
+                    opacity: _animationController,
+                    child: TextfieldNumberWidget(
+                      hintText: AppLocalizations.of(context)!.mobileNumber,
+                    ),
+                  ),
                 ),
               ),
             ),
@@ -141,8 +232,17 @@ Scaffold _portraitModeOnly(BuildContext context) {
               padding: EdgeInsets.symmetric(horizontal:20.0),
               child: Align(
                 alignment: Alignment.centerLeft,
-                child: ButtonWidget(
-                    buttonText: AppLocalizations.of(context)!.register,
+                child: SlideTransition(
+                  position: Tween<Offset>(
+                    begin: Offset(-1, 0),
+                    end: Offset.zero,
+                  ).animate(_animationController),
+                  child: FadeTransition(
+                    opacity: _animationController,
+                    child: ButtonWidget(
+                        buttonText: AppLocalizations.of(context)!.register,
+                    ),
+                  ),
                 ),
               ),
             ),
@@ -153,8 +253,17 @@ Scaffold _portraitModeOnly(BuildContext context) {
               padding: EdgeInsets.symmetric(horizontal:20.0),
               child: Align(
                 alignment: Alignment.centerLeft,
-                child: ClickableLink(
-                    clickableLinkText: AppLocalizations.of(context)!.alreadyHaveAccount,
+                child: SlideTransition(
+                  position: Tween<Offset>(
+                    begin: Offset(2, 0),
+                    end: Offset.zero,
+                  ).animate(_animationController),
+                  child: FadeTransition(
+                    opacity: _animationController,
+                    child: ClickableLink(
+                        clickableLinkText: AppLocalizations.of(context)!.alreadyHaveAccount,
+                    ),
+                  ),
                 ),
               ),
             ),
