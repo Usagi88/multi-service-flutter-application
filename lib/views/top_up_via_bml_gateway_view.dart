7f6398eb29b90c429827afbab68923f4eb2799e9
Usagi88 <ibrahimnaish5677@gmail.com>
2021-12-22 20:30:17 +0500

    topUpViaBMLGateway view animations

--- lib/views/top_up_via_bml_gateway_view.dart
+++ lib/views/top_up_via_bml_gateway_view.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:fpapp/provider/locale_provider.dart';
@@ -13,8 +15,34 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:fpapp/widgets/warning_text_widget.dart';
 import 'package:provider/provider.dart';
 
-class TopUpViaBMLGatewayView extends StatelessWidget {
+class TopUpViaBMLGatewayView extends StatefulWidget {
   const TopUpViaBMLGatewayView({Key? key}) : super(key: key);
+
+  @override
+  State<TopUpViaBMLGatewayView> createState() => _TopUpViaBMLGatewayViewState();
+}
+
+class _TopUpViaBMLGatewayViewState extends State<TopUpViaBMLGatewayView> with SingleTickerProviderStateMixin{
+  AnimationController? _animationController;
+  Tween<double> _tween = Tween(begin: 0.1, end: 1);
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+
+    );
+    Timer(Duration(milliseconds: 250), () => _animationController!.forward());
+    _animationController!.forward();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController!.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,12 +50,12 @@ class TopUpViaBMLGatewayView extends StatelessWidget {
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
-    return _portraitModeOnly(context);
+    return _portraitModeOnly(context,  _animationController, _tween);
   }
 }
 
 //portrait mode
-Scaffold _portraitModeOnly(BuildContext context) {
+Scaffold _portraitModeOnly(BuildContext context,  _animationController, _tween) {
   return Scaffold (
     appBar: NavbarWithBackButton(),
     drawer: SideMenu(),
@@ -51,6 +79,7 @@ Scaffold _portraitModeOnly(BuildContext context) {
                     bannerIcon: FontAwesomeIcons.wallet,
                     bannerAmount: 20320.20,
                     bannerPoints: 200,
+                    animationController: _animationController,
                   ),
                 ]
             ),
@@ -60,12 +89,21 @@ Scaffold _portraitModeOnly(BuildContext context) {
                   children: [
                     Container(
                       alignment: Alignment.centerLeft,
-                      child: Text(
-                        AppLocalizations.of(context)!.topUpViaBmlFirsText,
-                        style: TextStyle(
-                            fontSize: 14.0,
-                            fontWeight: FontWeight.w400,
-                            color: Color(0xff909090)
+                      child: SlideTransition(
+                        position: Tween<Offset>(
+                          begin: Offset(-1, 0),
+                          end: Offset.zero,
+                        ).animate(_animationController),
+                        child: FadeTransition(
+                          opacity: _animationController,
+                          child: Text(
+                            AppLocalizations.of(context)!.topUpViaBmlFirsText,
+                            style: TextStyle(
+                                fontSize: 14.0,
+                                fontWeight: FontWeight.w400,
+                                color: Color(0xff909090)
+                            ),
+                          ),
                         ),
                       ),
                     ),
@@ -74,10 +112,19 @@ Scaffold _portraitModeOnly(BuildContext context) {
                     ),
                     Align(
                       alignment: Alignment.centerLeft,
-                      child: Container(
-                        height: 1,
-                        width: MediaQuery.of(context).size.width * 0.25,
-                        color: Color(0xff909090),
+                      child: SlideTransition(
+                        position: Tween<Offset>(
+                          begin: Offset(-1, 0),
+                          end: Offset.zero,
+                        ).animate(_animationController),
+                        child: FadeTransition(
+                          opacity: _animationController,
+                          child: Container(
+                            height: 1,
+                            width: MediaQuery.of(context).size.width * 0.25,
+                            color: Color(0xff909090),
+                          ),
+                        ),
                       ),
                     ),
                   ]
@@ -90,7 +137,17 @@ Scaffold _portraitModeOnly(BuildContext context) {
                 alignment: Alignment.centerLeft,
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Text(AppLocalizations.of(context)!.amount, style: TextStyle(fontSize: 14,fontWeight: FontWeight.w400)),
+                  child: SlideTransition(
+                    position: Tween<Offset>(
+                      begin: Offset(-1, 0),
+                      end: Offset.zero,
+                    ).animate(_animationController),
+                    child: FadeTransition(
+                      opacity: _animationController,
+                        child: Text(AppLocalizations.of(context)!.amount, style: TextStyle(fontSize: 14,fontWeight: FontWeight.w400)
+                        )
+                    ),
+                  ),
                 )
             ),
             SizedBox(
@@ -98,8 +155,17 @@ Scaffold _portraitModeOnly(BuildContext context) {
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: TextfieldTextWidget(
-                  hintText: AppLocalizations.of(context)!.amount
+              child: SlideTransition(
+                position: Tween<Offset>(
+                  begin: Offset(2, 0),
+                  end: Offset.zero,
+                ).animate(_animationController),
+                child: FadeTransition(
+                  opacity: _animationController,
+                  child: TextfieldTextWidget(
+                      hintText: AppLocalizations.of(context)!.amount
+                  ),
+                ),
               ),
             ),
 
@@ -110,7 +176,17 @@ Scaffold _portraitModeOnly(BuildContext context) {
                 alignment: Alignment.centerLeft,
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Text(AppLocalizations.of(context)!.processingFee, style: TextStyle(fontSize: 14,fontWeight: FontWeight.w400)),
+                  child: SlideTransition(
+                    position: Tween<Offset>(
+                      begin: Offset(-1, 0),
+                      end: Offset.zero,
+                    ).animate(_animationController),
+                    child: FadeTransition(
+                      opacity: _animationController,
+                        child: Text(AppLocalizations.of(context)!.processingFee, style: TextStyle(fontSize: 14,fontWeight: FontWeight.w400)
+                        )
+                    ),
+                  ),
                 )
             ),
             SizedBox(
@@ -118,8 +194,17 @@ Scaffold _portraitModeOnly(BuildContext context) {
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: TextfieldTextWidget(
-                  hintText: AppLocalizations.of(context)!.processingFee
+              child: SlideTransition(
+                position: Tween<Offset>(
+                  begin: Offset(2, 0),
+                  end: Offset.zero,
+                ).animate(_animationController),
+                child: FadeTransition(
+                  opacity: _animationController,
+                  child: TextfieldTextWidget(
+                      hintText: AppLocalizations.of(context)!.processingFee
+                  ),
+                ),
               ),
             ),
 
@@ -130,7 +215,17 @@ Scaffold _portraitModeOnly(BuildContext context) {
                 alignment: Alignment.centerLeft,
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Text(AppLocalizations.of(context)!.total, style: TextStyle(fontSize: 14,fontWeight: FontWeight.w400)),
+                  child: SlideTransition(
+                    position: Tween<Offset>(
+                      begin: Offset(-1, 0),
+                      end: Offset.zero,
+                    ).animate(_animationController),
+                    child: FadeTransition(
+                      opacity: _animationController,
+                        child: Text(AppLocalizations.of(context)!.total, style: TextStyle(fontSize: 14,fontWeight: FontWeight.w400)
+                        )
+                    ),
+                  ),
                 )
             ),
             SizedBox(
@@ -138,8 +233,17 @@ Scaffold _portraitModeOnly(BuildContext context) {
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: TextfieldTextWidget(
-                  hintText: AppLocalizations.of(context)!.total
+              child: SlideTransition(
+                position: Tween<Offset>(
+                  begin: Offset(2, 0),
+                  end: Offset.zero,
+                ).animate(_animationController),
+                child: FadeTransition(
+                  opacity: _animationController,
+                  child: TextfieldTextWidget(
+                      hintText: AppLocalizations.of(context)!.total
+                  ),
+                ),
               ),
             ),
             SizedBox(
@@ -147,21 +251,39 @@ Scaffold _portraitModeOnly(BuildContext context) {
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: ButtonWidget(buttonText: AppLocalizations.of(context)!.topUpViaBmlButtonText),
+              child: SlideTransition(
+                position: Tween<Offset>(
+                  begin: Offset(-1, 0),
+                  end: Offset.zero,
+                ).animate(_animationController),
+                child: FadeTransition(
+                  opacity: _animationController,
+                    child: ButtonWidget(buttonText: AppLocalizations.of(context)!.topUpViaBmlButtonText)
+                ),
+              ),
             ),
             SizedBox(
               height: 10,
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Container(
-                height: 53,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(
-                        'assets/images/bml-gateway.png'),
-                    fit: BoxFit.fill,
+              child: SlideTransition(
+                position: Tween<Offset>(
+                  begin: Offset(2, 0),
+                  end: Offset.zero,
+                ).animate(_animationController),
+                child: FadeTransition(
+                  opacity: _animationController,
+                  child: Container(
+                    height: 53,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                            'assets/images/bml-gateway.png'),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
                   ),
                 ),
               ),
@@ -171,8 +293,17 @@ Scaffold _portraitModeOnly(BuildContext context) {
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Text(AppLocalizations.of(context)!.topUpViaBmlSecondText,
-                  style: TextStyle(fontSize: 14,fontWeight: FontWeight.w400, color: Colors.grey.shade600)
+              child: SlideTransition(
+                position: Tween<Offset>(
+                  begin: Offset(-1, 0),
+                  end: Offset.zero,
+                ).animate(_animationController),
+                child: FadeTransition(
+                  opacity: _animationController,
+                  child: Text(AppLocalizations.of(context)!.topUpViaBmlSecondText,
+                      style: TextStyle(fontSize: 14,fontWeight: FontWeight.w400, color: Colors.grey.shade600)
+                  ),
+                ),
               ),
             ),
             SizedBox(
@@ -180,10 +311,22 @@ Scaffold _portraitModeOnly(BuildContext context) {
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: WarningText(
-                warningText: AppLocalizations.of(context)!.topUpViaBmlThirdText,
-                warningTextBottomColor: 0xff909090
+              child: SlideTransition(
+                position: Tween<Offset>(
+                  begin: Offset(-1, 0),
+                  end: Offset.zero,
+                ).animate(_animationController),
+                child: FadeTransition(
+                  opacity: _animationController,
+                  child: WarningText(
+                    warningText: AppLocalizations.of(context)!.topUpViaBmlThirdText,
+                    warningTextBottomColor: 0xff909090
+                  ),
+                ),
               )
+            ),
+            SizedBox(
+              height: 20,
             ),
           ],
         ),
